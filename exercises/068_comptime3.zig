4fe16b5ddeea6f3a5f8f8027fab07943c75964de
ln2-kristhogub <lihasha2.kristhogubaduge@live.uwe.ac.uk>
2025-10-25 08:27:49 +0100

    Completed Ziglings exercises up to task 70

--- exercises/068_comptime3.zig
+++ exercises/068_comptime3.zig
@@ -43,7 +43,7 @@ const Schooner = struct {
         //
         // Please change this so that it sets a 0 scale to 1
         // instead.
-        if (my_scale == 0) @compileError("Scale 1:0 is not valid!");
+if (my_scale == 0) my_scale = 1;
 
         self.scale = my_scale;
         self.hull_length /= my_scale;
@@ -69,21 +69,17 @@ pub fn main() void {
     // Hey, we can't just pass this runtime variable as an
     // argument to the scaleMe() method. What would let us do
     // that?
-    var scale: u32 = undefined;
+   
 
-    scale = 32; // 1:32 scale
-
-    minnow.scaleMe(scale);
+    minnow.scaleMe(32);
     minnow.printMe();
 
-    scale -= 16; // 1:16 scale
 
-    shark.scaleMe(scale);
+    shark.scaleMe(16);
     shark.printMe();
 
-    scale -= 16; // 1:0 scale (oops, but DON'T FIX THIS!)
 
-    whale.scaleMe(scale);
+    whale.scaleMe(0);
     whale.printMe();
 }
 //
